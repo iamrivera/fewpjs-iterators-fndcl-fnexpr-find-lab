733c5de49f8deed0c30286669feb94caa350fcf1
iamrivera <a3m3rivera@gmail.com>
2021-07-16 21:55:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(array) {
+    let winningYear = array.find(function(object){return object['result'] === "W"})
+    if (winningYear) {
+        return winningYear.year
+    } else {
+        return winningYear
+    }
+}
